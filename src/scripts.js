4e81c2f5cfc5bdbeef96b47f07e99bb44c71d41d
Lia Zapciroiu <lzapciroiu@deloittece.com>
2021-06-28 11:06:26 +0300

    all words prod

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,15 @@
 import "./styles.scss";
-import {
-    pikachu
-} from "./eggs/pikachu";
-import {
-    floricica
-} from "./eggs/floricica";
+import { pikachu } from "./eggs/pikachu";
+import { music } from "./eggs/music";
+import socks from "./eggs/socks";
+import { floricica } from "./eggs/floricica";
 import duke from "./eggs/duke";
 import monkeys from './eggs/monkeys/monkeys'
 
 pikachu(window);
+music(window);
+socks();
 floricica(window);
 duke(window);
-monkeys(window)
+monkeys(window);
 
