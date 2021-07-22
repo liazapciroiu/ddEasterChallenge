a3ac30303de95e4a4e7accf1f59422d910728c19
Sandu <sandurares57@gmail.com>
2021-07-22 11:33:09 +0300

    start

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,14 @@
 import "./styles.scss";
 import { pikachu } from "./eggs/pikachu";
 import { music } from "./eggs/music";
+
 import socks from "./eggs/socks";
 import { floricica } from "./eggs/floricica";
 import duke from "./eggs/duke";
 import monkeys from './eggs/monkeys/monkeys'
 import cats from './eggs/cats';
 import { sportsmonth } from "./eggs/sportsmonth";
+import { manele } from "./eggs/manele";
 
 pikachu(window);
 music(window);
@@ -16,3 +18,4 @@ duke(window);
 monkeys(window);
 cats(window)
 sportsmonth(window);
+manele(window)
