15a7a59a02cc91ab2fb423bc155fe77630f08f70
Sandu <sandurares57@gmail.com>
2021-07-23 11:52:28 +0300

    add yt frame

--- src/eggs/manele.js
+++ src/eggs/manele.js
@@ -1,160 +1,168 @@
+/// helpers
+const getElement = (name) => document.querySelector("." + "m" + "-" + name);
+
+const createElement = (tag, className, styles) => {
+  const element = document.createElement(tag);
+  element.classList.add(
+    'm' + "-element",
+    ...className.split(" ").map((cName) => 'm' + "-" + cName)
+  );
+  styles &&
+    Object.entries(styles).forEach((style) => {
+      if (element.style[style[0]] !== undefined) {
+        element.style[style[0]] = style[1];
+      }
+    });
+  return element;
+};
+
 export function manele(window) {
-  // manele
-  const key = [77, 65, 78, 69, 76, 69];
-  let ck = 0;
-  const max = key.length;
-  let running = false;
+const manele = [
+  {
+    name: "Pupatura",
+    id: "u18KvwhxOyY",
+    start: 0,
+    end: 140,
+  },
+  {
+    name: "Petrekere",
+    id: "iFTYB5Em78w",
+    start: 0,
+    end: 115,
+  },
+  {
+    name: "Diamante",
+    id: "jyd81XVz1ZE",
+    start: 0,
+    end: 140,
+  },
+];
 
-  const songs = [
-    {
-      name: "Dragoste de inchiriat",
-      image: "https://i.postimg.cc/25Zxfyvw/Akcent-removebg-preview.png",
-      audio: "DragosteDeInchiriat.mp3",
-    },
-    {
-      name: "Dragostea din tei",
-      image: "https://i.postimg.cc/52p7nsgD/ozone-removebg-preview.png",
-      audio: "DragosteaDinTei.mp3",
-    },
-    {
-      name: "Haide vino inapoi",
-      image:
-        "https://i.postimg.cc/PrQ25SgN/im-d799af3821101c78f052ed7871fda05a-removebg-preview.png",
-      audio: "HaideVinoInapoi.mp3",
-    },
-    {
-      name: "Baietii si fetele",
-      image:
-        "https://i.postimg.cc/d0p9xpyN/Andra-la-debut-removebg-preview.png",
-      audio: "BaietiiSiFetele.mp3",
-    },
-    {
-      name: "In lipsa mea",
-      image:
-        "https://i.postimg.cc/L5BBhs0G/primul-album-smiley-in-lipsa-mea-poate-fi-ascultat-de-astazi-pe-canalul-de-youtube-al-artistului-1-s.png",
-      audio: "InLipsaMea.mp3",
-    },
-    {
-      name: "20 de ani",
-      image: "https://i.postimg.cc/pL1Qm4qV/V-59d09dc688-removebg-preview.png",
-      audio: "20DeAni.mp3",
-    },
-    {
-      name: "Daca dragostea dispare",
-      image: "https://i.postimg.cc/664p8J23/hqdefault-removebg-preview.png",
-      audio: "DacaDragosteaDispare.mp3",
-    },
-  ];
+// const key = [77, 65, 78, 69, 76, 69];
+const key = [77];
+const stopArr = []
 
-  const oldMusic = (random) => {
-    const bg = document.createElement("div");
-    const title = document.createElement("h1");
 
-    const discoBall = new Image();
-    discoBall.src =
-      "https://bestanimations.com/media/mirror-balls/645938848animated-disco-ball-5-2.gif";
-    discoBall.style.width = "200px";
-    discoBall.style.height = "200px";
-    discoBall.style.transition = "2s all";
-    discoBall.style.position = "fixed";
-    discoBall.style.left = "calc(50% - 125px)";
-    discoBall.style.top = "50px";
-    discoBall.style.zIndex = 999999;
-    discoBall.style.display = "none";
+let ck = 0;
+const max = key.length;
+const stop = stopArr.length;
 
-    const img = new Image();
-    img.src = songs[random].image;
-    img.style.width = "500px";
-    img.style.height = "350px";
-    img.style.transition = "2s all";
-    img.style.position = "fixed";
-    img.style.left = "calc(50% - 125px)";
-    img.style.bottom = "-350px";
-    img.style.zIndex = 999999;
+let running = false;
 
-    title.innerHTML = songs[random].name;
-    title.style.width = "300px";
-    title.style.textAlign = "center";
-    title.style.zIndex = 999999;
-    title.style.position = "fixed";
-    title.style.top = "10%";
-    title.style.left = "calc(50%-120px)";
+const getManea = () => {
+  // get random manea
+ let maneaNr = Math.floor(Math.random() * manele.length);
 
-    document.body.appendChild(title);
+  return manele[maneaNr];
+  
+};
 
-    const audio = new Audio(`../assets/audio/${songs[random].audio}`);
-    document.body.appendChild(discoBall);
-    document.body.appendChild(img);
-    audio.play();
 
-    window.setTimeout(function () {
-      img.style.bottom = "0px";
-    }, 50);
 
-    window.setTimeout(function () {
-      bg.style.width = "100%";
-      bg.style.height = "100%";
-      bg.style.left = 0;
-      bg.style.top = 0;
-      bg.style.position = "fixed";
-      bg.style.zIndex = 9999999;
-      bg.style.opacity = 0;
+const createVideo = (a) => {
+  const video = createElement('iframe', 'manele', {
+    aspectRatio: "16/9",
+    width: "auto",
+    height: "100%",
+    maxWidth: "95vw",
+    maxHeight: "70vh",
+  })
+  video.setAttribute(
+    "src",
+    "https://www.youtube.com/embed/" +
+      a.id +
+      "?start=" +
+      a.start +
+      "&end=" +
+      a.end +
+      "&autoplay=1&controls=0&disablekb=1&color=white&iv_load_policy=3&r=" +
+      new Date().getTime()
+  );
 
-      document.body.appendChild(bg);
+  video.setAttribute(
+    "allow",
+    "autoplay"
+  );
 
-      for (var x = 0; x < 220; x++) {
-        (function (x) {
-          window.setTimeout(function () {
-            discoBall.style.display = "block";
-            if (x % 2 === 0) {
-              bg.style.opacity = 0;
-            } else {
-              img.style.transform = `translate(${Math.floor(
-                Math.random() * 520 - 260
-              )}px, ${Math.floor(Math.random() * 520 - 260)}px)`;
-              bg.style.background =
-                "#" + Math.floor(Math.random() * 16777215).toString(16);
-              title.style.background =
-                "#" + Math.floor(Math.random() * 16777215).toString(16);
-              bg.style.opacity = 0.5;
-            }
-          }, x * 150);
-        })(x);
-      }
-    }, 1500);
+  return video;
 
-    window.setTimeout(function () {
-      img.style.bottom = "-350px";
-    }, 6500);
-    window.setTimeout(function () {
-      img.parentNode.removeChild(img);
-      bg.parentNode.removeChild(bg);
-      title.parentNode.removeChild(title);
-      discoBall.parentNode.removeChild(discoBall);
-      running = false;
-    }, 7000);
-  };
+};
 
-  const record = (e) => {
-    if (e.which === key[ck]) {
-      ck++;
-    } else {
-      ck = 0;
-    }
 
-    if (ck === max) {
-      if (!running) {
-        running = true;
-        oldMusic(Math.floor(Math.random() * 7));
-      }
+const maneaLayer = () => {
+  if (getElement("layer")) return;
 
-      ck = 0;
-    }
-  };
+  const layer = createElement("div", "layer", {
+    position: "fixed",
+    top: "5%",
+    left: "50%",
+    transform: "translateX(-50%)",
+    width: "30%",
+    height: "40%",
+    backgroundColor: "rgba(0,0,0,0)",
+    display: "flex",
+    alignItems: "center",
+    alignContent: "space-between",
+    justifyContent: "space-between",
+    flexDirection: "column",
+    boxSizing: "border-box",
+    transition: "1s all",
+  });
 
-  const init = () => {
-    document.addEventListener("keyup", record);
-  };
+  document.body.appendChild(layer);
+  setTimeout(() => {
+    layer.style.backgroundColor = "rgba(0,0,0,0.95)";
+  }, 1);
+};
 
-  init();
+
+const maneaPlayer = (sock) => {
+  if (getElement("manele")) return;
+  getElement("layer").appendChild(createVideo(sock));
+};
+
+const clearScene = () => {
+  Array.from(document.querySelectorAll("." + 'm' + "-element")).forEach(
+    (node) => node.remove()
+  );
+};
+const clearLayer = () => {
+  const layer = getElement("layer");
+  if (!layer) return;
+  layer.innerHTML = "";
+};
+
+
+const record = (e) => {
+  console.log(e.which)
+  if (e.which === key[ck]) {
+    ck++;
+  } else {
+    ck = 0;
+  }
+
+  if (ck === max) {
+    if (!running) {
+      running = true;
+      let a = getManea()
+      maneaLayer()
+      maneaPlayer(a)
+      console.log(createVideo(a))
+  
+     }
+    ck = 0;
+  } else if( ck === stop) {
+    running = false
+    clearLayer()
+    clearScene()
+
+  }
+};
+
+const init = () => {
+  document.addEventListener("keyup", record);
+};
+
+init();
+
 }
