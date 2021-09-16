e018fa1d0f62161149edabc70144fa825f12b055
IChristiann <criionita@deloittece.com>
2021-09-16 12:12:16 +0300

    dreams

--- src/scripts.js
+++ src/scripts.js
@@ -1,23 +1,25 @@
-import "./styles.scss";
-import { pikachu } from "./eggs/pikachu";
-import { music } from "./eggs/music";
+import './styles.scss'
+import { pikachu } from './eggs/pikachu'
+import { music } from './eggs/music'
 
-import socks from "./eggs/socks";
-import { floricica } from "./eggs/floricica";
-import duke from "./eggs/duke";
-import monkeys from "./eggs/monkeys/monkeys";
-import cats from "./eggs/cats";
-import { sportsmonth } from "./eggs/sportsmonth";
-import { manele } from "./eggs/manele";
-import coffee from "./eggs/coffee";
+import socks from './eggs/socks'
+import { floricica } from './eggs/floricica'
+import duke from './eggs/duke'
+import monkeys from './eggs/monkeys/monkeys'
+import cats from './eggs/cats'
+import { sportsmonth } from './eggs/sportsmonth'
+import { manele } from './eggs/manele'
+import coffee from './eggs/coffee'
+import { dreams } from './eggs/dreams'
 
-pikachu(window);
-music(window);
-socks();
-floricica(window);
-duke(window);
-monkeys(window);
-cats(window);
-sportsmonth(window);
-manele(window);
-coffee(window);
+pikachu(window)
+music(window)
+socks()
+floricica(window)
+duke(window)
+monkeys(window)
+cats(window)
+sportsmonth(window)
+manele(window)
+coffee(window)
+dreams(window)
